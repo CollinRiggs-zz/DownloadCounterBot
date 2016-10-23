8c440c1ee28fb59bd5dae357107f77dbb04b6853
Bread10 <CollinRiggs@users.noreply.github.com>
2016-10-23 15:44:09 -0400

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace DownloadBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyBot bot = new MyBot();
+        }
+
+    }
+}
